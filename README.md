8c10294f12f4e943b169d929afc35aaa5163ec28
Pedro de las Heras Quiros <pheras@gmail.com>
2017-12-04 20:59:19 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# spark-heroku-example
+
+Code for tutorial: [https://sparktutorials.github.io/2015/08/24/spark-heroku.html](https://sparktutorials.github.io/2015/08/24/spark-heroku.html)
+
+Modified to use postgresql in heroku (pheras):
+1) heroku addons:create heroku-postgresql:hobby-dev --app spark-pheras
+2) see pom.xml for postsgresql dependency
+3) see Main.java 
+
+Modified to be deployed in heroku through travis
+
+
---
+++ src/main/java/Main.java
@@ -0,0 +1,90 @@
+
+import static spark.Spark.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import java.net.URISyntaxException;
+import java.net.URI;
+
+public class Main {
+	
+	// heroku postgress
+	private static Connection getConnection() throws URISyntaxException, SQLException {
+	    URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+	    String username = dbUri.getUserInfo().split(":")[0];
+	    String password = dbUri.getUserInfo().split(":")[1];
+	    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+
+	    return DriverManager.getConnection(dbUrl, username, password);
+	}
+
+	public static String doWork() throws ClassNotFoundException, URISyntaxException {
+		String result = new String();
+		
+		// load the sqlite-JDBC driver using the current class loader
+//		Class.forName("org.sqlite.JDBC");
+
+		Connection connection = null;
+		try {
+			// create a database connection
+//			connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
+			connection = getConnection();
+			
+			Statement statement = connection.createStatement();
+//			statement.setQueryTimeout(30); // set timeout to 30 sec.
+
+			statement.executeUpdate("drop table if exists person");
+//			statement.executeUpdate("create table person (id integer, name string)");
+			statement.executeUpdate("create table person (id integer, name text)");
+
+			statement.executeUpdate("insert into person values(1, 'leo')");
+			statement.executeUpdate("insert into person values(2, 'yui')");
+			ResultSet rs = statement.executeQuery("select * from person");
+			while (rs.next()) {
+				// read the result set
+				result += "name = " + rs.getString("name") + "\n";
+				System.out.println("name = " + rs.getString("name"));
+				
+				result += "id = " + rs.getInt("id") + "\n";
+				System.out.println("id = " + rs.getInt("id"));
+			}
+		} catch (SQLException e) {
+			// if the error message is "out of memory",
+			// it probably means no database file is found
+			System.err.println(e.getMessage());
+		} finally {
+			try {
+				if (connection != null)
+					connection.close();
+			} catch (SQLException e) {
+				// connection close failed.
+				System.err.println(e);
+			}
+		}
+
+		return result;
+	}
+
+	public static void main(String[] args) throws ClassNotFoundException {
+        port(getHerokuAssignedPort());
+
+		// spark server
+		get("/hello", (req, res) -> doWork());
+
+	}
+	static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
+}
+
+
