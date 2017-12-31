3d6c41a1f2368d28b2e41bf940cae041e87b784d
Pedro de las Heras Quiros <pheras@gmail.com>
2017-12-31 15:26:49 +0100

    added compile tag to postgresql in pom

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -82,6 +82,7 @@ public class Main {
 
 
 	// heroku postgress
+
 	URI dbUri = new URI(System.getenv("DATABASE_URL"));
 	String username = dbUri.getUserInfo().split(":")[0];
 	String password = dbUri.getUserInfo().split(":")[1];
